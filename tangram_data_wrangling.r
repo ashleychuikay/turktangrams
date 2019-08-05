309cfdf6c4249082e2a3acd814e7c73e053702c3
ashleychuikay <30600210+ashleychuikay@users.noreply.github.com>
2019-08-05 11:40:59 -0500

    new norming analysis

--- tangram_data_wrangling.r
+++ tangram_data_wrangling.r
@@ -7,14 +7,48 @@ library(lubridate)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-filenames1 <- list.files("turkstudy1/production-results/", 
-                        pattern="*.json", full.names = TRUE)
-filenames2 <- list.files("turkstudy2/production-results/", 
+## old stimuli
+#filenames1 <- list.files("turkstudy1/production-results/", 
+#                        pattern="*.json", full.names = TRUE)
+#filenames2 <- list.files("turkstudy2/production-results/", 
+#                         pattern="*.json", full.names = TRUE)
+#filenames3 <- list.files("turkstudy3/production-results/", 
+#                         pattern="*.json", full.names = TRUE)
+
+filenames1 <- list.files("similar1/production-results/", 
                          pattern="*.json", full.names = TRUE)
-filenames3 <- list.files("turkstudy3/production-results/", 
+filenames2 <- list.files("similar2/production-results/", 
                          pattern="*.json", full.names = TRUE)
-files <- append(filenames1, filenames2)
-filenames <- append(files, filenames3)
+filenames3 <- list.files("similar3/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames4 <- list.files("similar4/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames5 <- list.files("similar5/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames6 <- list.files("similar6/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames7 <- list.files("similar7/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames8 <- list.files("similar8/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames9 <- list.files("similar9/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames10 <- list.files("similar10/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames11 <- list.files("similar11/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames12 <- list.files("similar12/production-results/", 
+                         pattern="*.json", full.names = TRUE)
+filenames13 <- list.files("similar13/production-results/", 
+                          pattern="*.json", full.names = TRUE)
+
+
+
+## old stimuli
+#files <- append(filenames1, filenames2)
+#filenames <- append(files, filenames3)
+
+filenames <- rbind(filenames1, filenames2, filenames3, filenames4, filenames5, filenames6, filenames7, filenames8, filenames9, filenames10, filenames11, filenames12, filenames13)
 
 ldf <- lapply(filenames, fromJSON)
 res <- lapply(ldf, summary)
@@ -53,4 +87,4 @@ sort(howLong$howLong)
 median(howLong$howLong)/60
 hist(howLong$howLong/60)
 
-write.csv(tangramdf, 'turktangramsdata.csv', row.names = F)                 
+write.csv(tangramdf, 'similartangramsdata.csv', row.names = F)                 
